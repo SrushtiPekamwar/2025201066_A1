d3d2f0cecec7aa94d3909af28c878d1308d2960a
Srushti Pekamwar <srushtipekamwar@Srushtis-MacBook-Air.local>
2025-08-06 08:59:50 +0530

    Handles issues related the flag values

--- 2025201066_A1_Q1.cpp
+++ 2025201066_A1_Q1.cpp
@@ -16,6 +16,7 @@ for flag 2:
 
 #include<unistd.h>
 #include<cstring>
+#include<cstdlib>
 
 
 void printOnConsole(const char *msg) {
@@ -31,7 +32,7 @@ void printOnConsole(const char *msg) {
 }
 
 void printCommandUsage() {
-    printOnConsole("\n-------------------------------Command usage-------------------------------\n");
+    printOnConsole("-------------------------------Command usage-------------------------------\n");
     printOnConsole("Blockwise reversal     : ./a.out <input_file> 0 <block_size>\n");
     printOnConsole("Full file reversal     : ./a.out <input_file> 1\n");
     printOnConsole("Partial range reversal : ./a.out <input_file> 2 <start_index> <end_index>\n");
@@ -39,11 +40,59 @@ void printCommandUsage() {
 }
 
 int main(int argc, char *argv[]) {
-    if(argc>3 || argc<1) {
-        printOnConsole("Wrong number of arguments\n");
+    if(argc<3) {
+        printOnConsole("Few arguments\n");
         printCommandUsage();
         return 1;
     }
-    int flag = *argv[2];
-    const char *filename = argv[1];
+
+    else if(argc>5) {
+        printOnConsole("Too many arguments\n");
+        printCommandUsage();
+        return 1;
+    }
+
+    // flag is integer but we get string type from the console 
+    int flag = atoi(argv[2]);
+
+    if(flag==0) {
+        if(argc!=4) {
+            printOnConsole("Wrong number of arguments\n");
+            printCommandUsage();
+            return 1;
+        }
+        else {
+
+        }
+    }
+
+    else if(flag==1) {
+        if(argc!=3) {
+            printOnConsole("Wrong number of arguments\n");
+            printCommandUsage();
+            return 1;
+        }
+        else {
+
+        }
+    }
+
+    else if(flag==2) {
+        if(argc!=5) {
+            printOnConsole("Wrong number of arguments\n");
+            printCommandUsage();
+            return 1;
+        } 
+        else {
+
+        }
+    }
+
+    else {
+        printOnConsole("Invalid flag value. Valid flags are 0,1,2\n");
+        printCommandUsage();
+        return 1;
+    }
+
+    return 0;
 }
